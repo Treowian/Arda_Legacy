dd8ece7e1cfc7015d8ce48a5e26e3fcf06b63e45
Treowian <odilon93@gmail.com>
2026-07-07 15:54:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 // js/main.js
-import { gameState } from './core/state.js';
+import { gameState } from './state.js';
 import { loadGame } from './core/save.js';
 import { initUI } from './ui/dom.js';
 import { initEngine } from './core/engine.js';
 import { initActions } from './ui/actions.js';
 import { initProjects } from './ui/projects.js';
 import { renderBuildings } from './ui/buildings.js';
+import { initDebug } from './ui/debug.js';
 
-// Exposer le state à la console pour tricher/tester (F12)
+// Rendre accessible globalement pour les manipulations directes via F12
 window.gameState = gameState;
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadGame();
-    initUI();
-    initActions();
-    initProjects();
-    renderBuildings();
-    initEngine();
+    loadGame();       // 1. Charger les cookies/Localstorage
+    initUI();         // 2. Mettre en cache l'interface
+    initActions();    // 3. Écouter le bouton Inspirer
+    initProjects();   // 4. Initialiser la logique des grands projets
+    renderBuildings(); // 5. Lancer l'évaluation des infrastructures
+    initDebug();      // 6. Activer le menu de triche
+    initEngine();     // 7. Lancer le temps (10s / an)
 });
