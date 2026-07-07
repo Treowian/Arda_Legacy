2b2466eded4c17b235783fcbf45fd2daaad2d5ca
Treowian <odilon93@gmail.com>
2026-07-07 11:51:44 +0200

    Update save.js

--- js/core/save.js
+++ js/core/save.js
@@ -20,6 +20,10 @@ export function loadGame() {
             Object.assign(gameState.state, parsed.state);
             Object.assign(gameState.resources, parsed.resources);
             Object.assign(gameState.population, parsed.population);
+            
+            // ✨ LA CORRECTION EST ICI : On force la fin de la pause au démarrage
+            gameState.state.is_paused = false;
+            
             console.log("📂 Partie chargée avec succès !");
         } catch (e) {
             console.error("Erreur lors du chargement de la sauvegarde", e);
